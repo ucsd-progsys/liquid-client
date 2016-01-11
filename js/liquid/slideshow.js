c6e94fdbdcfa685829972725ab8c37faa03a9743
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-01-11 15:24:57 -0800

    start plmw talk

--- js/liquid/slideshow.js
+++ js/liquid/slideshow.js
@@ -1,24 +1,47 @@
 $(function () {
+
   var currSlide = 0;
   var allSlides = $('.slide');
 
+
+  function updateSlides(nextSlide){
+    if (nextSlide !== currSlide) {
+        $(allSlides[currSlide])
+          .removeClass('active')
+          .addClass('inactive');
+        $(allSlides[nextSlide])
+          .removeClass('inactive')
+          .addClass('active');
+
+        currSlide = nextSlide;
+    }
+  }
+
   // Initialize: Hide all
-
-  $('.slide')
-    .removeClass('active')
-    .addClass('inactive');
-
-  $(allSlides[currSlide])
-    .removeClass('inactive')
-    .addClass('active');
+  $('.slide').removeClass('active').addClass('inactive');
+  $(allSlides[currSlide]).removeClass('inactive').addClass('active');
 
   // Update
-
-  $(document).on('keydown', function (event) {
+  $('.prevbutton').click(function (event) {
     var nextSlide = currSlide;
+    console.log('prev slide click');
+    nextSlide = currSlide ? currSlide - 1 : currSlide;
+    updateSlides(nextSlide);
+    event.preventDefault();
+   });
 
-    // https://css-tricks.com/snippets/javascript/javascript-keycodes/
-    
+  $('.nextbutton').click(function (event) {
+    var nextSlide = currSlide;
+    console.log('next slide click');
+    nextSlide = currSlide < allSlides.length - 1 ? currSlide + 1 : currSlide;
+    updateSlides(nextSlide);
+    event.preventDefault();
+   });
+
+ 
+  // https://css-tricks.com/snippets/javascript/javascript-keycodes/
+  /*
+  $(document).on('keydown', function (event) {
     // page up
     if (event.keyCode === 33) { 
       console.log('previous slide');
@@ -28,18 +51,7 @@ $(function () {
       console.log('next slide');
       nextSlide = currSlide < allSlides.length - 1 ? currSlide + 1 : currSlide;
     }
-
-    if (nextSlide !== currSlide) {
-
-      $(allSlides[currSlide])
-        .removeClass('active')
-        .addClass('inactive');
-      $(allSlides[nextSlide])
-        .removeClass('inactive')
-        .addClass('active');
-
-      currSlide = nextSlide;
-    }
-    event.preventDefault();
   });
+  */
+
 });
